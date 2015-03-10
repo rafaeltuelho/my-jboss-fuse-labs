35793bc3f8f8b58c77356a224a11cace0532901d
rafaeltuelho <rafaelcba@gmail.com>
2015-03-10 00:18:35 -0300

    deleting some projects

---
+++ fuse-fab-deployment/README.md
@@ -0,0 +1,3 @@
+# fuse-fab-deployment
+
+Show how to use the FAB package deployment model in JBoss Fuse
--- fuse-war-deployment/cxf-code-first-spring/src/main/java/br/net/rafaeltuelho/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package br.net.rafaeltuelho;
-
-import javax.jws.WebService;
-
-@WebService
-public interface HelloWorld {
-    String sayHi(String text);
-}
-
